e8b2094473fb2ea6405fbdefbe098d1054e7f230
jtyers <40065647+jtyers@users.noreply.github.com>
2020-11-10 10:57:13 +0000

    feat: more detailed inspector scripts

--- inspect-wrapper.sh
+++ inspect-wrapper.sh
@@ -2,9 +2,15 @@
 
 set -eu
 
+onExit() {
+  rm -rf "$extractDir" $scanFile
+}
+
 bucket="aaas-test-cloudmapper-results"
-extractDir="$PWD/account-data"
+extractDir="$(mktemp -d)"
 selector="fzy"
+scanFile="scan.tgz"
+scanFileUpdated="scan-updated.tgz"
 
 choices=$( aws s3 ls "s3://$bucket/results/" )
 chosenAccount=$(
@@ -19,15 +25,26 @@ chosenScan=$(
     | $selector --prompt="Choose scan: "
 )
 
-aws s3 cp "s3://$bucket/$chosenScan" ./scan.tgz
+aws s3 cp "s3://$bucket/$chosenScan" $scanFile
 
 [ -d "$extractDir" ] && rm -r "$extractDir"
 mkdir "$extractDir"
-tar -xzf ./scan.tgz -C "$extractDir"
+tar -xzf $scanFile -C "$extractDir"
 
 docker run --rm -it \
-  -v "$extractDir":/opt/cloudmapper/account-data \
+  --name cloudmapper-scanner-inspector \
+  -v "$extractDir/account-data":/opt/cloudmapper/account-data \
+  -v "$extractDir/web":/opt/cloudmapper/web \
   -p 8000:8000 \
   -e ACCOUNT_ID="$chosenAccount" \
+  "$@" \
   cloudmapper-scanner-inspector
 
+tar czf $scanFileUpdated -C $extractDir account-data web
+
+if [ "$(md5sum $scanFile)" != "$(md5sum $scanFileUpdated)" ]; then
+  echo "data was changed, updating s3" >&2
+  aws s3 cp $scanFileUpdated "s3://$bucket/$chosenScan" 
+fi
+
+trap onExit err exit
--- inspector/bin/inspect.sh
+++ inspector/bin/inspect.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -eu
 
-chosenAccount="${ACCOUNT_ID}"
+account="${ACCOUNT_ID}"
 
-echo "configuring cloudmapper for account $chosenAccount"
-python3 cloudmapper.py configure add-account --id $chosenAccount --name $chosenAccount
+c="python3 cloudmapper.py"
+echo "configuring cloudmapper for account $account"
+$c configure add-account --id $account --name $account
 
-python3 cloudmapper.py prepare --account $chosenAccount
-python3 cloudmapper.py weboftrust --accounts $chosenAccount
+$c prepare --account $account
+$c weboftrust --accounts $account
+
+$c report --accounts $account
+$c iam_report --accounts $account
+
+$c audit --accounts $account --json > web/account-data/audit.json || true
+$c find_admins  --accounts $account --json > web/account-data/find_admins.json || true
+$c find_unused  --accounts $account > web/account-data/find_unused.json || true
+$c public       --accounts $account > web/account-data/public.json || true
 
 python3 cloudmapper.py webserver --public >/dev/null &
 
